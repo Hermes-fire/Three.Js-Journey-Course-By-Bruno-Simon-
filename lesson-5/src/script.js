1a742c83ccae520ea05352c7415cd09c6e4fe74f
Hermes-fire <amine.sadali@gmail.com>
2022-08-29 19:56:48 +0100

    position/rotation

--- lesson-5/src/script.js
+++ lesson-5/src/script.js
@@ -10,18 +10,34 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1,1,1)
 const material = new THREE.MeshBasicMaterial({color: 0xff0000})
 const mesh = new THREE.Mesh(geometry, material)
+
+mesh.position.set(2,1,1)
+mesh.scale.set(2,1,1)
+//mesh.rotation.reorder('YXZ')
+mesh.rotation.y = Math.PI * 0.9
+mesh.rotation.x = Math.PI * 0.2
 scene.add(mesh)
+
+
+//Show Axes
+const axesHelper = new THREE.AxesHelper( 4 );
+scene.add( axesHelper );
 
 //Sizes
 const sizes = {
-    width: 800,
-    height: 600
+    width: 400,
+    height: 400
 }
 
 //Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+//camera.position.z = 3
+camera.position.set(5,5,5)
 scene.add(camera)
+
+camera.lookAt(mesh.position)
+
+//mesh.position.normalize()
 
 //renderer
 
